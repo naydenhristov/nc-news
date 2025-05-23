165d6b27686f3d5f402f1aac1ae331ec3ed99f15
naydenhristov <nhristov21@gmail.com>
2025-05-23 04:03:00 +0100

    task 7 - Vote on an article completed

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from "react";
-import { getArticleById, getCommentsByArticleId } from "../api";
+import { getArticleById, getCommentsByArticleId, updateArticleById } from "../api";
 import { useParams } from "react-router";
 import icon from "../assets/Thumb-up.png";
 
 export const ArticleCard = () => {
   const [article, setArticle] = useState([]);
   const [comments, setComments] = useState([]);
+  const [votes, setVotes] = useState(0);
+  const [votesMessage, setVotesMessage] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const [hasVoted, setHasVoted] = useState(false);
   const article_id = useParams().article_id;
+
+  function handleClickVote() {
+    event.preventDefault();
+    if (!hasVoted) { 
+      setVotes(votes + 1);
+      setHasVoted(true);
+      updateArticleById(article_id).then((updatedArticle) => {
+        console.log(updatedArticle, "<---updatedArticle");
+      } )
+      .catch((err) => {
+        console.log(err);
+      } ); 
+    } else {
+      setVotesMessage("You have already voted!");
+    }
+  }
 
   useEffect(() => {
     setLoading(true);
     getArticleById(article_id)
       .then((article) => {
         setArticle(article);
+        setVotes(article.votes);
         getCommentsByArticleId(article_id)
           .then((commentsList) => {
             setComments(commentsList);
@@ -56,7 +75,11 @@ export const ArticleCard = () => {
       <p>{article.body}</p>
       <div className="info-line">
         <img className="icon" alt="Thumb-up icon" src={icon} />
-        <span className="votes">Votes: {article.votes}</span>
+        <span className="votes">Votes: {votes}</span>
+        <button value="1" onClick={handleClickVote}>
+          Vote
+        </button>
+        <span className="votes">{votesMessage}</span>
       </div>
       <div className="comments">
         <span className="comment-span">Comments: {commentsCount}</span>
@@ -65,14 +88,16 @@ export const ArticleCard = () => {
       <div>
         {comments.map((comment) => {
           const commentDate = new Date(comment.created_at).toDateString();
-          return <div key={comment.comment_id} className="comment-card">
-            <p className="comment-body">{comment.body}</p>
-          <div className="comment-line">
-            <span className="comment-span">{commentDate}</span>
-            <span className="comment-span">Author: {comment.author}</span>
-            <button className="button">Delete Comment</button>
+          return (
+            <div key={comment.comment_id} className="comment-card">
+              <p className="comment-body">{comment.body}</p>
+              <div className="comment-line">
+                <span className="comment-span">{commentDate}</span>
+                <span className="comment-span">Author: {comment.author}</span>
+                <button className="button">Delete Comment</button>
+              </div>
             </div>
-          </div>;
+          );
         })}
       </div>
     </section>
